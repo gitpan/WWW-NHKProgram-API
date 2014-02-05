03313e0bf0c01f75d90715c4629a9cb8322fe032
Taiki Kawakami <moznion@gmail.com>
2014-02-05 14:44:59 +0000

    Import of MOZNION/WWW-NHKProgram-API-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-NHKProgram-API
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MOZNION/WWW-NHKProgram-API-0.02.tar.gz
    gitpan-cpan-author:       MOZNION
    gitpan-cpan-maturity:     released

--- lib/WWW/NHKProgram/API.pm
+++ lib/WWW/NHKProgram/API.pm
@@ -13,7 +13,7 @@ use Class::Accessor::Lite::Lazy (
     ro_lazy => [qw/provider/],
 );
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub list {
     my $self = shift;
@@ -140,7 +140,7 @@ Get program list.
         date    => '2014-02-04',
     });
 
-And following the same (you must C<use utf8;>);
+And following the same;
 
     my $program_list = $client->list({
         area    => '東京',
@@ -164,7 +164,7 @@ Get program list by genre.
         date    => '2014-02-04',
     });
 
-Yes! you can also specify following when you C<use utf8>;
+Yes! you can also specify following;
 
     my $genre_list = $client->genre({
         area    => '東京',
